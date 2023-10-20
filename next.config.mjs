f01b16c16d4f6afced50dc75f7d1318ae97ecd8a
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-20 22:56:00 +0200

    feat: added invite route to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/require-await */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -6,6 +7,16 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  async redirects() {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/api/oauth2/authorize?client_id=1029899066911490158&permissions=413390915648&scope=applications.commands%20bot",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
