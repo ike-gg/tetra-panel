9a8a6c0b3b205712d4865499f2fbc1cff99adacf
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-20 17:30:57 +0200

    feat: added discord to remote images next

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.discordapp.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default config;
