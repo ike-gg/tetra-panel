28fba50edd55385b89925066bbcb9201410309ec
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-22 05:02:51 +0200

    feat: usefeedback hook

---
+++ src/hooks/useFeedback.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const useFeedback = () => {
+  const [loading, setLoadingState] = useState(false);
+  const [error, setErrorState] = useState<string>();
+  const [success, setSuccessState] = useState<string>();
+
+  const setError = (textError: string) => {
+    setLoadingState(false);
+    setSuccessState(undefined);
+    setErrorState(textError);
+  };
+
+  const setSuccess = (testSuccess: string) => {
+    setLoadingState(false);
+    setErrorState(undefined);
+    setSuccessState(testSuccess);
+  };
+
+  const setLoading = (state: boolean) => {
+    setSuccessState(undefined);
+    setErrorState(undefined);
+    setLoadingState(state);
+  };
+
+  return {
+    loading,
+    error,
+    success,
+    setError,
+    setSuccess,
+    setLoading,
+  };
+};
