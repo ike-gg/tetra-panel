255528170f8670bc4e787038ba1855efc7ddd3c4
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-23 00:21:25 +0200

    feat: updated error route

--- src/app/(panel)/error.tsx
+++ src/app/(panel)/error.tsx
@@ -1,13 +1,42 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+} from "~/components/ui/dialog";
+import { routes } from "~/constants/routes";
+
 export default function TaskError({
   error,
 }: {
   error: Error & { digset?: string };
 }) {
+  const router = useRouter();
+
   return (
-    <div className="flex h-full items-center justify-center">
-      {error.message}
+    <div className="mx-auto flex h-full max-w-lg flex-col items-end justify-center gap-4">
+      <Dialog
+        defaultOpen={true}
+        onOpenChange={(p) => !p && router.push(routes.panel)}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogHeader>Something went wrong</DialogHeader>
+            <DialogDescription>{error.message}</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button>Go back</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
