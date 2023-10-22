2d5518d864b89d11ec77986e1387b273a71e87eb
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-22 05:02:43 +0200

    feat: arraybuffer base64 helpers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,37 @@ export function firstLetters(input: string) {
     .map((w) => w.at(0))
     .join("");
 }
+
+export function arrayBufferToBase64(arrayBuffer: ArrayBuffer): string {
+  const uint8Array = new Uint8Array(arrayBuffer);
+  let binaryString = "";
+  for (const byte of uint8Array) {
+    binaryString += String.fromCharCode(byte);
+  }
+  const base64String = btoa(binaryString);
+  return base64String;
+}
+
+export function base64ToArrayBuffer(base64: string) {
+  const binaryString = atob(base64);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+export const fileToBase64 = (file: File): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const result = reader.result;
+      if (!result || typeof result !== "string") {
+        reject();
+        return;
+      }
+      resolve(result);
+    };
+    reader.onerror = reject;
+  });
