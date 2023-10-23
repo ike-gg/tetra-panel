c7953fb7d89df4f80b5f16b8bcc58a773133d4f6
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-23 19:51:26 +0200

    feat: state to store managed guilds

---
+++ src/app/store/emoteContextStore.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { type PartialGuild } from "discord-oauth2";
+import { create } from "zustand";
+import { endpoints } from "~/constants/apiroutes";
+
+interface ContextGuilds {
+  name: string;
+  id: string;
+  icon?: string | null;
+}
+
+interface EmoteContextState {
+  fetch: () => Promise<void>;
+  guilds: ContextGuilds[] | null;
+}
+
+export const useEmoteContextStore = create<EmoteContextState>()((set) => ({
+  fetch: async () => {
+    try {
+      const request = await fetch(endpoints.getUserGuilds, {
+        credentials: "include",
+      });
+      const response = (await request.json()) as {
+        managingGuilds: PartialGuild[];
+      };
+      set({
+        guilds: response.managingGuilds.map((guild) => ({
+          icon: guild.icon,
+          id: guild.id,
+          name: guild.name,
+        })),
+      });
+    } catch (error) {
+      set({ guilds: [] });
+    }
+  },
+  guilds: null,
+}));
