a449cce7e3110a20ee07715cfe485c5e2062e072
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-23 19:51:12 +0200

    chore: constants of api endpoints

--- src/constants/apiroutes.ts
+++ src/constants/apiroutes.ts
@@ -5,7 +5,12 @@ const apiurl = env.NEXT_PUBLIC_BOTAPI_URL;
 const endpoints = {
   getUserGuilds: apiurl + "/guilds",
   getGuildEmotes: (id: string) => `${apiurl}/guilds/${id}/emotes`,
+
   task: (id: string) => `${apiurl}/tasks/${id}`,
+
+  addEmoteToGuild: (guildId: string) => apiurl + `/guilds/${guildId}`,
+  removeEmoteFromGuild: (emoteId: string, guildId: string) =>
+    apiurl + `/guilds/${guildId}/${emoteId}`,
 };
 
 export { endpoints };
