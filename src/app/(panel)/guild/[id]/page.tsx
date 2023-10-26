5a4236e2f062dadba92e02c1bf5187c84adcb1c0
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-26 07:52:58 +0200

    fix: guild id route handling 429 http

--- src/app/(panel)/guild/[id]/page.tsx
+++ src/app/(panel)/guild/[id]/page.tsx
@@ -1,39 +1,71 @@
 import { cookies } from "next/headers";
 import { type GuildEmoji } from "discord.js";
-import Image from "next/image";
 import { endpoints } from "~/constants/apiroutes";
+import { Emote } from "~/components/emotes/Emote";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { firstLetters, getGuildBanner, getGuildIcon } from "~/lib/utils";
+import { TypographyH2 } from "~/components/ui/typography";
+import { Input } from "~/components/ui/input";
+import { Search } from "~/components/ui/search";
 
 export default async function GuildIdPage({
   params,
+  searchParams: { search },
 }: {
   params: { id: string };
+  searchParams: { search: string };
 }) {
   const { id } = params;
-  const emotesRequest = await fetch(endpoints.getGuildEmotes(id), {
+
+  const request = await fetch(endpoints.getGuild(id), {
     headers: { Cookie: cookies().toString() },
-    cache: "force-cache",
   });
 
-  const { emotes } = (await emotesRequest.json()) as { emotes: GuildEmoji[] };
+  if (request.status === 429) {
+    throw "slow down";
+  }
+
+  if (!request.ok) {
+    const { error } = (await request.json()) as { error: string };
+    throw new Error(error);
+  }
+
+  const { emotes, name, icon, banner } = (await request.json()) as {
+    emotes: GuildEmoji[];
+    name: string;
+    icon: string | null;
+    banner: string | null;
+  };
 
   return (
-    <div>
-      Emotes of this guild:
-      <div className="grid grid-cols-16">
-        {emotes?.map((emote) => {
-          const { name, url } = emote;
-          return (
-            <div key={emote.id}>
-              <p>{name}</p>
-              <Image
-                width={96}
-                height={96}
-                src={url}
-                alt={`emoji named ${name}`}
-              />
-            </div>
-          );
-        })}
+    <div className="space-y-5">
+      <TypographyH2 className="flex items-center gap-2">
+        <Avatar className="h-16 w-16">
+          {icon && <AvatarImage src={getGuildIcon(id, icon)} />}
+          <AvatarFallback>{firstLetters(name)}</AvatarFallback>
+        </Avatar>
+        Emotes of {name}
+      </TypographyH2>
+      <Search />
+      <div className="grid grid-cols-7 gap-1 md:grid-cols-12 md:gap-3 lg:grid-cols-16">
+        {emotes
+          ?.filter((emote) => {
+            const emoteName = emote.name?.toLowerCase() ?? "";
+            const searchQuery = search?.toLowerCase() ?? "";
+            return emoteName.includes(searchQuery);
+          })
+          .map((emote) => (
+            <Emote
+              key={emote.id}
+              details={{
+                emoteName: emote.name ?? "Emote",
+                emoteUrl: emote.url,
+                origin: "DISCORD",
+                reference: emote.id,
+              }}
+              guildId={id}
+            />
+          ))}
       </div>
     </div>
   );
