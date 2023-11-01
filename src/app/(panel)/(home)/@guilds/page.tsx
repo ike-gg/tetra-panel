a410201327b634317afc46229356c0eabad5da57
ike <67127723+ike-gg@users.noreply.github.com>
2023-11-01 07:15:52 +0100

    feat: catching guilds error fetching

--- src/app/(panel)/(home)/@guilds/page.tsx
+++ src/app/(panel)/(home)/@guilds/page.tsx
@@ -1,59 +1,78 @@
 import { type PartialGuild } from "discord-oauth2";
 import { cookies } from "next/headers";
 import { GuildButton } from "~/components/guilds/GuildButton";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 import { TypographyH2 } from "~/components/ui/typography";
 import { endpoints } from "~/constants/apiroutes";
 
 export default async function PageGuikdsHomeParallel() {
-  const request = await fetch(endpoints.getUserGuilds, {
-    headers: { Cookie: cookies().toString() },
-    cache: "force-cache",
-  });
+  try {
+    const request = await fetch(endpoints.getUserGuilds, {
+      headers: { Cookie: cookies().toString() },
+      cache: "force-cache",
+    });
 
-  const { guilds, managingGuilds, guildsMissingTetra } =
-    (await request.json()) as {
-      guilds: PartialGuild[];
-      managingGuilds: PartialGuild[];
-      guildsMissingTetra: PartialGuild[];
-    };
+    if (!request.ok) {
+      const response = (await request.json()) as Record<string, string>;
+      throw new Error(
+        `Request failed with status ${request.status} (${JSON.stringify(
+          response,
+        )})`,
+      );
+    }
 
-  return (
-    <div className="flex flex-col gap-4">
-      {managingGuilds && managingGuilds.length > 0 && (
-        <div>
-          <TypographyH2>Managed servers</TypographyH2>
-          <div className="flex flex-wrap gap-2">
-            {managingGuilds?.map((guild) => (
-              <GuildButton guild={guild} key={guild.id} />
-            ))}
+    const { guilds, managingGuilds, guildsMissingTetra } =
+      (await request.json()) as {
+        guilds: PartialGuild[];
+        managingGuilds: PartialGuild[];
+        guildsMissingTetra: PartialGuild[];
+      };
+
+    return (
+      <div className="flex flex-col gap-4">
+        {managingGuilds && managingGuilds.length > 0 && (
+          <div>
+            <TypographyH2>Managed servers</TypographyH2>
+            <div className="flex flex-wrap gap-2">
+              {managingGuilds?.map((guild) => (
+                <GuildButton guild={guild} key={guild.id} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-      {guilds && guilds.length > 0 && (
-        <div>
-          <TypographyH2>Tetra-Connected servers</TypographyH2>
-          <div className="flex flex-wrap gap-2">
-            {guilds?.map((guild) => (
-              <GuildButton variant="outline" guild={guild} key={guild.id} />
-            ))}
+        )}
+        {guilds && guilds.length > 0 && (
+          <div>
+            <TypographyH2>Tetra-Connected servers</TypographyH2>
+            <div className="flex flex-wrap gap-2">
+              {guilds?.map((guild) => (
+                <GuildButton variant="outline" guild={guild} key={guild.id} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-      {guildsMissingTetra && guildsMissingTetra.length > 0 && (
-        <div>
-          <TypographyH2>Add a bot</TypographyH2>
-          <div className="flex flex-wrap gap-2">
-            {guildsMissingTetra?.map((guild) => (
-              <GuildButton
-                asInvite
-                variant="outline"
-                guild={guild}
-                key={guild.id}
-              />
-            ))}
+        )}
+        {guildsMissingTetra && guildsMissingTetra.length > 0 && (
+          <div>
+            <TypographyH2>Invite Tetra</TypographyH2>
+            <div className="flex flex-wrap gap-2">
+              {guildsMissingTetra?.map((guild) => (
+                <GuildButton
+                  asInvite
+                  variant="outline"
+                  guild={guild}
+                  key={guild.id}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  } catch (error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>Fetching guilds failed</AlertTitle>
+        <AlertDescription>{String(error)}</AlertDescription>
+      </Alert>
+    );
+  }
 }
--- src/app/(panel)/(home)/@savedemotes/loading.tsx
+++ src/app/(panel)/(home)/@savedemotes/loading.tsx
@@ -2,13 +2,26 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export default function LoadingTasksHomeParallel() {
   return (
-    <>
-      <Skeleton className="h-6 w-32" />
-      <div className="flex gap-4 overflow-x-hidden">
-        <Skeleton className="h-24 w-64 border border-neutral-200 shadow-lg" />
-        <Skeleton className="h-24 w-80 border border-neutral-200 shadow-lg" />
-        <Skeleton className="h-24 w-72 border border-neutral-200 shadow-lg" />
+    <div className="space-y-3">
+      <Skeleton className="h-10 w-48" />
+      <div className="flex flex-wrap gap-4">
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-28 w-28" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-28 w-28" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-28 w-28" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-28 w-28" />
+          <Skeleton className="h-4 w-full" />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
