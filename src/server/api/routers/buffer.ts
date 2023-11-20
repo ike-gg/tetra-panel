bc3b1da5eac37ff3d16eb549e232049a7cf656b7
ike <67127723+ike-gg@users.noreply.github.com>
2023-11-20 05:40:24 +0100

    feat: trpc is back

--- src/server/api/routers/buffer.ts
+++ src/server/api/routers/buffer.ts
@@ -1,28 +1,24 @@
-// import { z } from "zod";
-// import { createTRPCRouter, publicProcedure } from "../trpc";
-// import { base64ToArrayBuffer } from "~/lib/utils";
-// import sharp from "sharp";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-// export const bufferRouter = createTRPCRouter({
-//   buffer: publicProcedure
-//     .input(z.object({ base64: z.string() }))
-//     .query(async ({ input }) => {
-//       const { base64 } = input;
-//       const arrayBuffer = base64ToArrayBuffer(base64);
-//       const sharpInstance = sharp(arrayBuffer, { animated: false });
-//       const metadata = await sharpInstance.metadata();
-//       return metadata;
-//     }),
-//   get: publicProcedure
-//     .input(z.object({ url: z.string().url() }))
-//     .query(async ({ input }) => {
-//       const { url } = input;
-//       try {
-//         const request = await fetch(url);
-//         const arrayBuffer = await request.arrayBuffer();
-//         return arrayBuffer;
-//       } catch (e) {
-//         throw new Error(String(e));
-//       }
-//     }),
-// });
+export const bufferRouter = createTRPCRouter({
+  get: publicProcedure
+    .input(z.object({ url: z.string().url() }))
+    .query(async ({ input }) => {
+      const { url } = input;
+      try {
+        const request = await fetch(url);
+        const arrayBuffer = await request.arrayBuffer();
+        return arrayBuffer;
+      } catch (e) {
+        throw new Error(String(e));
+      }
+    }),
+
+  sperma: publicProcedure
+    .input(z.object({ url: z.string().url() }))
+    .mutation(({ input }) => {
+      const { url } = input;
+      return url;
+    }),
+});
