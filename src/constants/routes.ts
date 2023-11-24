81ab33a4553aa1898fb6f7b182d33943167a5a53
ike <67127723+ike-gg@users.noreply.github.com>
2023-11-24 23:54:34 +0100

    feat: library route fn

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -10,6 +10,16 @@ const guilds = {
   id: (id: string) => `/guild/${id}`,
 } as const;
 
-const routes = { guilds, tasks, auth, panel };
+const library = (query?: string, page?: number, provider?: string) => {
+  const searchParams: string[] = [];
+
+  query && searchParams.push(`query=${query}`);
+  page && searchParams.push(`page=${page}`);
+  provider && searchParams.push(`provider=${provider}`);
+
+  return `/library${searchParams.length ? `?${searchParams.join("&")}` : ""}`;
+};
+
+const routes = { guilds, tasks, auth, panel, library };
 
 export { routes };
