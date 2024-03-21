190b0782edebc3e740b8418d2adf41f3982e85b6
ike-gg <67127723+ike-gg@users.noreply.github.com>
2024-03-22 00:53:17 +0100

    feat: new loading spinner

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,16 +1,26 @@
+import "../../styles/spinner.css";
 import { cn } from "~/lib/utils";
-
 interface Props {
   className?: string;
 }
 
 export const Spinner = ({ className }: Props) => {
   return (
-    <div
-      className={cn(
-        "size-4 animate-spin rounded-full border-[2px] border-neutral-800 !border-t-transparent",
-        className,
-      )}
-    />
+    <div className={cn("size-4", className)}>
+      <div className={"relative left-1/2 top-1/2 size-full"}>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+      </div>
+    </div>
   );
 };
