fd1b7719be2ccabd1d55384e987057a1e584fff2
ike-gg <67127723+ike-gg@users.noreply.github.com>
2024-03-22 18:28:51 +0100

    feat: spinner style update

--- src/app/(panel)/(home)/page.tsx
+++ src/app/(panel)/(home)/page.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 
+import { Spinner } from "~/components/ui/spinner";
+
 export default function Home() {
-  return <div className="flex items-center gap-2 mix-blend-difference"></div>;
+  return (
+    <div className="flex items-center gap-2 mix-blend-difference">
+      <Spinner />
+    </div>
+  );
 }
--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -8,18 +8,18 @@ export const Spinner = ({ className }: Props) => {
   return (
     <div className={cn("size-4", className)}>
       <div className={"relative left-1/2 top-1/2 size-full"}>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
-        <div className="spinner_bar absolute left-[-12.5%] top-[-3.9%] h-[8%] w-[25%] bg-neutral-700"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
+        <div className="spinner_bar bg-neutral-500"></div>
       </div>
     </div>
   );
